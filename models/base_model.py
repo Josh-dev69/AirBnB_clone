959b7e45fd9ea8daaff35a20a5c22bd5e165066e
Fikayomi <fikayoogundijo@gmail.com>
2023-05-14 08:43:49 -0700

    base

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,8 @@
 """Defining a class"""
 from _datetime import datetime
 import uuid
-from models.engine.file_storage import storage
+#from models import storage
+
 
 class BaseModel:
     """Representing a BaseModel class"""
@@ -18,16 +19,14 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        value = datetime.strptime(value,
+                                                  "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            
 
     def __str__(self):
         """Return string representation of the class"""
@@ -37,7 +36,7 @@ class BaseModel:
     def save(self):
         """Update the updated_at with current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        
 
     def to_dict(self):
         """Return a dict containing all key and value of dict"""
