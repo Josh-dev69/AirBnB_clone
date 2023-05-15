105dbb309a29ec8305f59ca7adf8a05c7de79887
Josh-dev69 <ebubejoshua69@gmail.com>
2023-05-15 00:19:54 -0700

    Updated file

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Defining a class"""
 from _datetime import datetime
 import uuid
+from models import storage
 
 class BaseModel:
     """Representing a BaseModel class"""
@@ -24,6 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return string representation of the class"""
@@ -33,6 +35,8 @@ class BaseModel:
     def save(self):
         """Update the updated_at with current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
+
     def to_dict(self):
         """Return a dict containing all key and value of dict"""
         my_dict = self.__dict__.copy()
@@ -40,3 +44,4 @@ class BaseModel:
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
         return my_dict
+
